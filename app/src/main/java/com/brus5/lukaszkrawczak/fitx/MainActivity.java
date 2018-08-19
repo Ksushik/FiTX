f804131ffacd2f4cc26dc3f3a5eb5f81f33b9a97
LukaszKrawczakPC <lukasz634@gmail.com>
2018-08-19 18:54:24 +0200

    Fixing bugs

--- app/src/main/java/com/brus5/lukaszkrawczak/fitx/MainActivity.java
+++ app/src/main/java/com/brus5/lukaszkrawczak/fitx/MainActivity.java
@@ -2,11 +2,8 @@ package com.brus5.lukaszkrawczak.fitx;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
