5f46b5b89042d1d363e429cee7cbe34cce36f2e5
Łukasz Krawczak <lukasz634@gmail.com>
2018-09-05 21:43:14 +0200

    Refactoring code

--- app/src/main/java/com/brus5/lukaszkrawczak/fitx/converter/TimeStamp.java
+++ app/src/main/java/com/brus5/lukaszkrawczak/fitx/converter/TimeStamp.java
@@ -1,5 +1,9 @@
 package com.brus5.lukaszkrawczak.fitx.converter;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class TimeStamp
 {
     private String dateToday;
@@ -16,10 +20,16 @@ public class TimeStamp
      * If for example user choosed not today date, this method must
      *
      * @return
-     * @throws NullPointerException
      */
-    public String getNewTimeStamp() throws NullPointerException
+    public String getNewTimeStamp()
     {
+        if (oldTimeStamp == null || dateToday == null)
+        {
+            dateToday = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
+            oldTimeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+        }
+
         return this.oldTimeStamp.replace(oldTimeStamp.substring(5, 10), dateToday.substring(5, 10));
     }
 }
