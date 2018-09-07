119be57866607d5c18cffcd5fcd5813ac5d6fa01
Łukasz Krawczak <lukasz634@gmail.com>
2018-09-07 14:20:24 +0200

    Refactoring code

---
+++ app/src/main/java/com/brus5/lukaszkrawczak/fitx/IDefaultView.java
@@ -0,0 +1,14 @@
+package com.brus5.lukaszkrawczak.fitx;
+
+public interface IDefaultView
+{
+    /**
+     * Loading Views.
+     */
+    void loadInput();
+
+    /**
+     * Loading default Views for every activity
+     */
+    void loadDefaultView();
+}
--- app/src/main/java/com/brus5/lukaszkrawczak/fitx/IPreviousActivity.java
+++ app/src/main/java/com/brus5/lukaszkrawczak/fitx/IPreviousActivity.java
@@ -1,4 +1,10 @@
 package com.brus5.lukaszkrawczak.fitx;
 
+/**
+ * This is Interface where should all methods be implemented,
+ * when Activity must load informations from previous Activity.
+ */
 public interface IPreviousActivity
-{}
+{
+
+}
