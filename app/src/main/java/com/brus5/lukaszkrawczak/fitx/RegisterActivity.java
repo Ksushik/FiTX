b9c4a60a545652ac5d79da0e3e95c5307d91791d
Łukasz <krawczak.lukasz@gmail.com>
2018-10-18 19:11:59 +0200

    Adding menu inflater to Activity

--- app/src/main/java/com/brus5/lukaszkrawczak/fitx/RegisterActivity.java
+++ app/src/main/java/com/brus5/lukaszkrawczak/fitx/RegisterActivity.java
@@ -2,14 +2,40 @@ package com.brus5.lukaszkrawczak.fitx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity
 {
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register_save_user,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.menu_save_user:
+                Log.i(TAG, "onOptionsItemSelected: menu_save_user clicked!");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
